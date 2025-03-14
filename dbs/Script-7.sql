e0847aa636c7ef2c60232627b03858120074aaa6
Mary Ann <tomasswaier@gmail.com>
2025-03-14 18:32:49 +0100

    uwu

--- dbs/Script-7.sql
+++ dbs/Script-7.sql
@@ -1,6 +1,7 @@
-select * from play_records pr limit 100;
+select * from play_records pr order by id;
 select * from players pr limit 100;
-
+select * from games g  order by id limit 100;
+select * from teams t limit 100;
 
 
 --first
@@ -17,15 +18,42 @@ WHERE prev_event_type = 'REBOUND' AND event_msg_type = 'FIELD_GOAL_MADE'AND play
 ORDER BY period ASC,period_time DESC,player_id ASC;
 
 --two -- in progress
-
-WITH newTable AS (
-    SELECT UNNEST(ARRAY[player1_id, player2_id]) AS player_id,unnest(ARRAY[player1_team_id, player2_team_id]) as team_id,
-           game_id, event_number, event_msg_type,event_msg_action_type, period, wctimestring,pctimestring,score,score_margin
-    FROM play_records
-    WHERE event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')
-) select 
-distinct player_id,count(distinct team_id ) as team_count
-from(
-SELECT * FROM newTable 
-where player_id is not null and team_id is not null)group by player_id
-;
+WITH player_teams AS (
+    SELECT UNNEST(ARRAY[player1_id, player2_id]) AS player_id,UNNEST(ARRAY[player1_team_id, player2_team_id]) AS team_id, -- put two meows into one for ez pz uwu
+	game_id,event_msg_type,p.id
+    FROM play_records p 
+    JOIN games g ON p.game_id = g.id 
+    WHERE event_msg_type IN ('FREE_THROW', 'FIELD_GOAL_MADE', 'FIELD_GOAL_MISSED', 'REBOUND')AND g.season_id = '22017'
+),
+team_changes AS ( -- count num of meow changes
+    SELECT player_id,team_id,
+	COUNT(DISTINCT game_id) AS games_played FROM player_teams 
+    WHERE player_id IS NOT NULL AND team_id IS NOT NULL 
+    GROUP BY player_id, team_id
+),
+player_change_counts AS ( -- select only those meowzers who changed more than once
+    SELECT player_id,COUNT(team_id) AS team_change_count
+    FROM team_changes GROUP BY player_id HAVING COUNT(team_id) > 2
+),
+ppg_tab AS ( -- errro r somewhere in here idk where
+    SELECT player_id,team_id,
+	ROUND(COUNT(*) FILTER (WHERE pt.event_msg_type = 'FIELD_GOAL_MADE' and pt.player_id=pr.player1_id) * 2.0 / 
+	COUNT(DISTINCT pt.game_id), 2) AS ppg
+    FROM player_teams pt join play_records as pr on pt.id=pr.id
+    WHERE pt.event_msg_type = 'FIELD_GOAL_MADE'GROUP BY pt.player_id, team_id
+),
+apg_tab AS ( -- errro r somewhere in here idk where
+    SELECT player_id,team_id,
+	ROUND(COUNT(*) FILTER (WHERE pt.event_msg_type = 'FIELD_GOAL_MADE' and pt.player_id=pr.player2_id) * 2.0 / 
+	COUNT(DISTINCT pt.game_id), 2) AS apg
+    FROM player_teams pt join play_records as pr on pt.id=pr.id
+    WHERE pt.event_msg_type = 'FIELD_GOAL_MADE'GROUP BY pt.player_id, team_id
+)
+SELECT pcc.player_id,p.first_name,p.last_name,tc.team_id,t.full_name,ppg.ppg,apg.apg,tc.games_played
+FROM player_change_counts pcc
+JOIN team_changes tc ON pcc.player_id = tc.player_id
+JOIN players p ON tc.player_id = p.id
+JOIN teams t ON tc.team_id = t.id
+LEFT JOIN ppg_tab ppg ON tc.player_id = ppg.player_id AND tc.team_id = ppg.team_id
+LEFT JOIN apg_tab apg ON tc.player_id = apg.player_id AND tc.team_id = apg.team_id
+ORDER BY tc.player_id ASC, tc.team_id ASC;
