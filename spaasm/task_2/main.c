be3d33c045a7056f590713f9da5a44536e385e71
Alice Liddle <tomasswaier@gmail.com>
2025-04-19 14:23:16 +0200

    spasm

--- spaasm/task_2/main.c
+++ spaasm/task_2/main.c
@@ -15,16 +15,17 @@
 
 #define MAX_CONNECTIONS 5
 int PORT = 3000;
-int NUM = 0;
 int server_fd;
 int TIMEOUT = 10;
 int VERBOSE = 0;
+char *IP = NULL;
 
 void help();
 int run_server();
 int run_client();
 
 void debug_print(const char *format, ...) {
+  // function for task -v
   if (!VERBOSE)
     return;
 
@@ -34,6 +35,7 @@ void debug_print(const char *format, ...) {
   va_end(args);
 }
 int change_dir_call(char **line_args) {
+  // needed for proper functionality of program
   if (line_args[1] == NULL) {
     perror("missing second argument");
   } else {
@@ -44,12 +46,11 @@ int change_dir_call(char **line_args) {
 
   return 1;
 }
-void execute_child(char **line_args) {
-  //  kinder
+void execute_child(char **line_args) { //  kinder
   char *input = NULL, *output = NULL;
   char **clean_args = malloc(128 * sizeof(char *));
   int idx = 0;
-
+  // checking if something needs contains > or <
   for (int i = 0; line_args[i]; i++) {
     if (strcmp(line_args[i], "<") == 0) {
       input = line_args[++i];
@@ -59,6 +60,7 @@ void execute_child(char **line_args) {
       clean_args[idx++] = line_args[i];
     }
   }
+  // if program does contain < or > I give that arg priority
   clean_args[idx] = NULL;
   if (input) {
     int fd = open(input, O_RDONLY);
@@ -78,29 +80,36 @@ void execute_child(char **line_args) {
     close(fd);
   }
   execvp(clean_args[0], clean_args);
-  perror("execvp");
   free(clean_args);
+  perror("execvp");
   exit(EXIT_FAILURE);
 }
+
 int external_call(char **line_args) {
+  // function that activates whenever we call something not defined in our
+  // program
   debug_print("[DEBUG] Executing command: %s\n", line_args[0]);
   pid_t pid;
+  // splits into 2
   pid = fork();
   if (pid > 0) {
     // eltern
     waitpid(pid, NULL, 0);
   } else if (pid == 0) {
+    // child gets executed
     execute_child(line_args);
   }
   return 1;
 }
 int execute_exit_call() {
+  // function that quits program
   printf("halt");
   fflush(stdout);
   kill(getppid(), SIGUSR1);
   exit(1);
 }
 void handle_shutdown(int sig) {
+  // in case we need
   if (server_fd > 0)
     close(server_fd);
 
@@ -110,6 +119,7 @@ void handle_shutdown(int sig) {
 }
 
 int execute_args(char **line_args) {
+  // main function that handles acts differently based on input commands given
   if (strcmp(line_args[0], "cd") == 0) {
     change_dir_call(line_args);
   } else if (strcmp(line_args[0], "help") == 0) {
@@ -128,25 +138,27 @@ int execute_args(char **line_args) {
 }
 
 char **devide_line(char *line_read, int *index, char *delimiter) {
-  char *token;
-  char **tokens = (char **)malloc(128 * sizeof(char *));
+  // splits line based on some delimiter
+  char *not_token;
+  char **not_tokens = (char **)malloc(128 * sizeof(char *));
 
-  if (!tokens) {
+  if (!not_tokens) {
     perror("allocation error");
     exit(1);
   }
-  token = strtok(line_read, delimiter);
+  not_token = strtok(line_read, delimiter);
   *index = 0;
-  while (token != NULL && *index < 31) {
-    tokens[*index] = token;
+  while (not_token != NULL && *index < 31) {
+    not_tokens[*index] = not_token;
     (*index)++;
-    token = strtok(NULL, delimiter);
+    not_token = strtok(NULL, delimiter);
   }
-  tokens[*index] = NULL;
-  return tokens;
+  not_tokens[*index] = NULL;
+  return not_tokens;
 }
 
 char *read_line() {
+  // receives user input
   char *buffer = NULL;
   size_t bufsize = 0;
   if (getline(&buffer, &bufsize, stdin) == -1) {
@@ -156,6 +168,7 @@ char *read_line() {
   return buffer;
 }
 char *remove_comment(char *line_read) {
+  // removes commends using strchr
   char *pos = strchr(line_read, '#');
   char *new_line;
   if (pos != NULL) {
@@ -166,6 +179,7 @@ char *remove_comment(char *line_read) {
       exit(1);
     }
     strncpy(new_line, line_read, pos - line_read);
+    // add \0 at the end bcs strings
     new_line[pos - line_read] = '\0';
   } else {
     new_line = strdup(line_read);
@@ -174,17 +188,20 @@ char *remove_comment(char *line_read) {
   return new_line;
 }
 int main_loop(int client_socket) {
+  // main forloop for receiving commands
   char *name = getlogin();
   int status = 1;
   fd_set read_fds;
   struct timeval timeout;
 
   while (status) {
-    printf("%s %d >", name, NUM);
+    time_t now = time(NULL);
+    printf("%s %s>", name, ctime(&now));
     fflush(stdout);
 
     FD_ZERO(&read_fds);
     FD_SET(STDIN_FILENO, &read_fds);
+    // remember time to keep track of timeout
     timeout.tv_sec = TIMEOUT;
     timeout.tv_usec = 0;
 
@@ -193,18 +210,19 @@ int main_loop(int client_socket) {
       perror("select");
       break;
     } else if (activity == 0) {
+      // in case of no activity from user close his port
       printf("\nNo input for %d seconds. Closing connection.\n", TIMEOUT);
       break;
     }
-
+    // receive line
     char *line_read = read_line();
     if (!line_read)
       break;
-
+    // adjust the line
     line_read = remove_comment(line_read);
     int num_commands;
     char **commands = devide_line(line_read, &num_commands, ";");
-
+    // execute every part devided by ; separately
     for (int i = 0; i < num_commands; i++) {
       int num_args;
       char **args = devide_line(commands[i], &num_args, " ");
@@ -221,9 +239,17 @@ int main_loop(int client_socket) {
 }
 void help() {
   printf("Autor:Tomáš Meravý Murárik\nThis program should be used as "
-         "normal shell\ncommands: cd,ls,help,quit,halt\n");
+         "normal shell\ncommands: cd,ls,help,quit,halt\n\t-s\tstarts program "
+         "in server mode\n\t-c\tstarts program in client mode\n\t-p "
+         "[num]\tworking port to work on\n\t-t [num]\ttime to "
+         "timeout\t\t(6b)\n\t-v\tallow debug error printing\t(1b)\n\t-l "
+         "[file]\twrites logs into a file (2b)\t\n\t-f\treads commands from "
+         "file\t(2b)\n\t-x [arg]\texecutes program given to it as "
+         "arg.(substitude for -c [arg] (2b)\n\t-i [ip]\twill only accpe "
+         "connections from IP (2b)");
 }
 void execute_script(const char *filename) {
+  // code for -f part
   FILE *script = fopen(filename, "r");
   if (!script) {
     perror("fopen");
@@ -246,6 +272,7 @@ void execute_script(const char *filename) {
 
 int main(int argc, char *argv[]) {
   int (*run_func)() = &main_loop;
+  // read args from user
   for (int i = 1; i < argc; i++) {
     if (argc > 1) {
       if (strcmp(argv[i], "-h") == 0) {
@@ -264,6 +291,34 @@ int main(int argc, char *argv[]) {
         exit(1);
       } else if (strcmp(argv[i], "-l") == 0 && i + 1 < argc) {
         freopen(argv[i + 1], "a", stdout);
+      } else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
+        IP = argv[i + 1]; // Store the IP as a STRING
+        i++;
+      } else if (strcmp(argv[i], "-x") == 0 && i + 1 < argc) {
+        int dummy;
+        char *cmd = argv[i + 1];
+        // run single command
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        struct sockaddr_in serv_addr = {.sin_family = AF_INET,
+                                        .sin_port = htons(PORT)};
+        inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) {
+          perror("Connection failed");
+          exit(1);
+        }
+
+        // Send command
+        write(sock, cmd, strlen(cmd));
+        write(sock, "\n", 1);
+
+        // Read response
+        char buffer[1024];
+        ssize_t n;
+        while ((n = read(sock, buffer, sizeof(buffer) - 1)) > 0) {
+          buffer[n] = '\0';
+          printf("%s", buffer);
+        }
       }
 
       if (strcmp(argv[i], "-s") == 0) {
@@ -288,11 +343,13 @@ int run_server() {
   int opt = 1;
   int addrlen = sizeof(address);
 
+  // create socket
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
     perror("socket failed");
     exit(EXIT_FAILURE);
   }
 
+  // set up socket
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
                  sizeof(opt))) {
     perror("setsockopt");
@@ -300,7 +357,15 @@ int run_server() {
   }
 
   address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
+
+  if (IP) {
+    if (inet_pton(AF_INET, IP, &address.sin_addr) <= 0) {
+      perror("invalid IP address");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    address.sin_addr.s_addr = INADDR_ANY;
+  }
   address.sin_port = htons(PORT);
 
   if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
@@ -308,6 +373,7 @@ int run_server() {
     exit(EXIT_FAILURE);
   }
 
+  // start it
   if (listen(server_fd, MAX_CONNECTIONS) < 0) {
     perror("listen");
     exit(EXIT_FAILURE);
@@ -330,7 +396,6 @@ int run_server() {
 
     debug_print("[DEBUG] New connection (socket %d)\n", new_socket);
     pid_t pid = fork();
-    NUM++;
     if (pid < 0) {
       perror("fork failed");
       close(new_socket);
@@ -361,6 +426,7 @@ int run_client() {
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
 
+  // set up ip
   if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
     perror("invalid address");
     return EXIT_FAILURE;
